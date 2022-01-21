3e35f53bbd05efda0e1f3ff0edca982f1079de16
Maloney1988 <98157400+Maloney1988@users.noreply.github.com>
2022-01-21 14:34:51 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"os"
+
+	
+)
+
+func main() {
+    str := os.Args[1]
+    if str=="" || str=="''"{
+    	fmt.Println(0)
+    } else{
+		masStr := strings.Split(str, " ")
+		fmt.Println(len(masStr))
+ 	}
+	
+		
+}
+
